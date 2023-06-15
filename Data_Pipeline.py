310d4705415c9b8f04d413f3d21dd19d52894987
Chalanthon Ainthachot <135719155+Chaland0@users.noreply.github.com>
2023-06-15 21:16:07 +0700

    Update Data_Pipeline.py

--- Data_Pipeline.py
+++ Data_Pipeline.py
@@ -1,6 +1,7 @@
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.utils.dates import days_ago
 import pandas as pd
 import requests
